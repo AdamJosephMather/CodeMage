cf2ba65f40823df8017f780b6adf88df7e1aeffa
Adam Joseph Mather <adamjosephmather@gmail.com>
2025-05-28 12:01:13 -0600

    Testing push

--- CodeMage.go
+++ CodeMage.go
@@ -287,7 +287,7 @@ func main() {
 		case *tcell.EventKey:
 			if ev.Key() == tcell.KeyEscape {
 				return
-			}
+			} // to test git push
 			
 			if current_window == "edit"{
 				handleKey(s, ev)
